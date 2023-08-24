f811f2e48143f4bbef789ede6e937f774f3eff67
Wolves <cstingl@protonmail.com>
2023-08-24 08:34:31 -0400

    Initial match solution w/o main

--- match/match.go
+++ match/match.go
@@ -1,1 +1,62 @@
 package match
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type matcher struct {
+	input  io.Reader
+	output io.Writer
+	text   string
+}
+
+type option func(m *matcher) error
+
+func WithInput(input io.Reader) option {
+	return func(m *matcher) error {
+		if input == nil {
+			return errors.New("nil input reader")
+		}
+		m.input = input
+		return nil
+	}
+}
+func WithOutput(output io.Writer) option {
+	return func(m *matcher) error {
+		if output == nil {
+			return errors.New("nil output writer")
+		}
+		m.output = output
+		return nil
+	}
+}
+
+func NewMatcher(opts ...option) (*matcher, error) {
+	m := &matcher{
+		input:  os.Stdin,
+		output: os.Stdout,
+	}
+	for _, opt := range opts {
+		err := opt(m)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return m, nil
+}
+
+func (m *matcher) MatchingLines(s string) {
+	lines := bufio.NewScanner(m.input)
+	for lines.Scan() {
+		//Check Matches
+		if strings.Contains(lines.Text(), s) {
+			fmt.Fprintln(m.output, lines.Text())
+		}
+	}
+}
--- match/match_test.go
+++ match/match_test.go
@@ -1,1 +1,27 @@
 package match_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/wolves/match"
+)
+
+func TestMatchingLines_PrintsLinesWithArgMatchToOutput(t *testing.T) {
+	t.Parallel()
+	inBuf := bytes.NewBufferString("match line 1\nregular line 2\nanothermatch 3")
+	outBuf := new(bytes.Buffer)
+	m, err := match.NewMatcher(
+		match.WithInput(inBuf),
+		match.WithOutput(outBuf),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	m.MatchingLines("match")
+	want := "match line 1\nanothermatch 3\n"
+	got := outBuf.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
