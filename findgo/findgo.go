908396e0f37ad7ee57582af8514a85f43c9fe9d6
Wolves <cstingl@protonmail.com>
2023-08-29 09:34:31 -0400

    Initial solution and passing findgo test

--- findgo/findgo.go
+++ findgo/findgo.go
@@ -1,1 +1,19 @@
 package findgo
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func Files(p string) []string {
+	files := []string{}
+	fsys := os.DirFS(p)
+	fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
+		if filepath.Ext(p) == ".go" {
+			files = append(files, p)
+		}
+		return nil
+	})
+	return files
+}
--- findgo/findgo_test.go
+++ findgo/findgo_test.go
@@ -1,1 +1,22 @@
 package findgo_test
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/wolves/findgo"
+)
+
+func TestFilesCorrectylListsFileInTree(t *testing.T) {
+	t.Parallel()
+	want := []string{
+		"file.go",
+		"subfolder/subfolder.go",
+		"subfolder2/another.go",
+		"subfolder2/file.go",
+	}
+	got := findgo.Files("testdata/tree")
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
