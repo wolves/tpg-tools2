94df55da4d4b198a8da4953317557efe082d5670
Wolves <cstingl@protonmail.com>
2023-08-29 16:39:02 -0400

    Add benchmarks for testing on filesystem vs in-mem

--- findgo/findgo_test.go
+++ findgo/findgo_test.go
@@ -9,6 +9,27 @@ import (
 	"github.com/wolves/findgo"
 )
 
+func BenchmarkFilesOnDisk(b *testing.B) {
+	fsys := os.DirFS("testing/tree")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = findgo.Files(fsys)
+	}
+}
+
+func BenchmarkFilesInMemory(b *testing.B) {
+	fsys := fstest.MapFS{
+		"file.go":                {},
+		"subfolder/subfolder.go": {},
+		"subfolder2/another.go":  {},
+		"subfolder2/file.go":     {},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = findgo.Files(fsys)
+	}
+}
+
 func TestFilesCorrectylListsFileInTree(t *testing.T) {
 	t.Parallel()
 	fsys := os.DirFS("testdata/tree")
